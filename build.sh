fdde84f33968fe5264495f51ee2b2ecea8985924
akande56 <abdulsalamabubakar52@gmail.com>
2024-09-29 18:00:27 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements/production.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 # echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('admin@gmail.com', 'abdul123.')"| python manage.py shell
+    
